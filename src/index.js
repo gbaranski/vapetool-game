a67af7bbe34b4c0ba1c65d2a0a1666c4c5a0e256
gbaranski <gbaranski19@gmail.com>
2020-04-28 18:45:47 +0200

    added falling objects

--- src/index.js
+++ src/index.js
@@ -19,13 +19,30 @@ class Bunny {
         container.addChild(this.bunny);
         app.ticker.add(delta => gameState.gameLoop(delta));
     }
-    move(dx) {
-        this.bunny.vx = dx;
-    }
     jump() {
         this.bunny.vy = -10;
     }
 };
+class FallingObject {
+    constructor() {
+        this.texture = PIXI.Texture.from('src/assets/eliquid.png');
+        this.fallingObjects = [];
+        this.objectGravity = 3;
+    }
+    create() {
+        for (let i = 0; i < 2; i++) {
+            this.fallingObjects[i] = new PIXI.Sprite(this.texture);
+            this.fallingObjects[i].scale.x = 0.1;
+            this.fallingObjects[i].scale.y = 0.1;
+            this.fallingObjects[i].x = Math.floor(Math.random() * (app.renderer.width))
+            this.fallingObjects[i].y = 0;
+            this.fallingObjects[i].vy = this.objectGravity;
+            container.addChild(this.fallingObjects[i]);
+        }
+    }
+
+}
+
 
 class GameState {
     constructor() {
@@ -40,6 +57,20 @@ class GameState {
         } else {
             bunny.bunny.vy = 0;
         }
+        fallingObject.fallingObjects.forEach(element => {
+            if (element.y <= app.renderer.view.height - element.height) {
+                element.y += element.vy;
+            } else {
+                element.vy = 0;
+            }
+            if (hitTestRectangle(bunny.bunny, element)) {
+                console.log("hit")
+                container.removeChild(element);
+                fallingObject.fallingObjects = fallingObject.fallingObjects.filter(function (e) {
+                    return e !== element;
+                });
+            }
+        });
 
     }
     handleKeyboardPress() {
@@ -115,8 +146,60 @@ class GameState {
         return key;
     }
 };
+
+function hitTestRectangle(r1, r2) {
+
+    //Define the variables we'll need to calculate
+    let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+    //hit will determine whether there's a collision
+    hit = false;
+
+    //Find the center points of each sprite
+    r1.centerX = r1.x + r1.width / 2;
+    r1.centerY = r1.y + r1.height / 2;
+    r2.centerX = r2.x + r2.width / 2;
+    r2.centerY = r2.y + r2.height / 2;
+
+    //Find the half-widths and half-heights of each sprite
+    r1.halfWidth = r1.width / 2;
+    r1.halfHeight = r1.height / 2;
+    r2.halfWidth = r2.width / 2;
+    r2.halfHeight = r2.height / 2;
+
+    //Calculate the distance vector between the sprites
+    vx = r1.centerX - r2.centerX;
+    vy = r1.centerY - r2.centerY;
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+    combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+    //Check for a collision on the x axis
+    if (Math.abs(vx) < combinedHalfWidths) {
+
+        //A collision might be occurring. Check for a collision on the y axis
+        if (Math.abs(vy) < combinedHalfHeights) {
+
+            //There's definitely a collision happening
+            hit = true;
+        } else {
+
+            //There's no collision on the y axis
+            hit = false;
+        }
+    } else {
+
+        //There's no collision on the x axis
+        hit = false;
+    }
+
+    //`hit` will be either `true` or `false`
+    return hit;
+};
 gameState = new GameState();
 bunny = new Bunny();
+fallingObject = new FallingObject();
 
 document.body.appendChild(app.view);
 const container = new PIXI.Container();
@@ -126,3 +209,7 @@ app.stage.addChild(container);
 bunny.texture.baseTexture.on('loaded', function () {
     bunny.create();
 });
+fallingObject.texture.baseTexture.on('loaded', function () {
+    fallingObject.create();
+
+})
