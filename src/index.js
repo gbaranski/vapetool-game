0b5e0b4aa1300c2aae6ec795e9e2f2392af53305
gbaranski <gbaranski19@gmail.com>
2020-04-28 17:51:54 +0200

    fixed jumping, working in game loop to hold 60fps

--- src/index.js
+++ src/index.js
@@ -4,101 +4,125 @@ const app = new PIXI.Application({
     backgroundColor: 0x1099bb,
     resolution: window.devicePixelRatio || 1,
 });
-
 class Bunny {
     constructor() {
         this.texture = PIXI.Texture.from('src/assets/bunny.png');
         this.bunny = new PIXI.Sprite(this.texture);
+        this.jumping = false;
     }
     create() {
         console.log(this.bunny);
         this.bunny.x = app.renderer.view.width / 2;
         this.bunny.y = app.renderer.view.height - this.bunny.height;
+        this.bunny.vx = 0;
+        this.bunny.vy = 0;
         container.addChild(this.bunny);
+        app.ticker.add(delta => gameState.gameLoop(delta));
     }
     move(dx) {
-        this.bunny.x += dx / 2;
+        this.bunny.vx = dx;
     }
-    jump(y) {
-        if (this.bunny.y == app.renderer.view.height - this.bunny.height) {
-            const jumpInterval = setInterval(() => {
-                this.bunny.y = this.bunny.y - 1;
-                if (this.bunny.y == y) {
-                    clearInterval(jumpInterval);
-                    const rotateInterval = setInterval(() => {
-                        this.bunny.angle += 5;
-                        if (this.bunny.angle > 360) {
-                            clearInterval(rotateInterval);
-                            this.bunny.angle = 0;
-                            const fallInterval = setInterval(() => {
-                                this.bunny.y = this.bunny.y + 1;
-                                if (this.bunny.y == app.renderer.view.height - this.bunny.height) {
-                                    clearInterval(fallInterval);
-                                }
-                            }, 1);
-                        }
-                    }, 1);
-                }
-            }, 1);
-        } else {
-            console.log("Bunny already in air");
-        }
+    jump() {
+        this.bunny.vy = -10;
     }
 };
+
+class GameState {
+    constructor() {
+        this.gravity = 0.5;
+    }
+    gameLoop() {
+        this.handleKeyboardPress();
+        bunny.bunny.x += bunny.bunny.vx;
+        bunny.bunny.y += bunny.bunny.vy;
+        if (bunny.bunny.y <= app.renderer.view.height - bunny.bunny.height) {
+            bunny.bunny.vy += this.gravity;
+        } else {
+            bunny.bunny.vy = 0;
+        }
+
+    }
+    handleKeyboardPress() {
+        let keyA = this.keyboard("a");
+        keyA.press = () => {
+            bunny.bunny.vx = -10;
+        }
+        keyA.release = () => {
+            if (bunny.bunny.vx = -10)
+                bunny.bunny.vx = 0;
+        }
+        let keyD = this.keyboard("d");
+        keyD.press = () => {
+            bunny.bunny.vx = 10;
+        }
+        keyD.release = () => {
+            if (bunny.bunny.vx = 10)
+                bunny.bunny.vx = 0;
+        }
+
+        let keyW = this.keyboard("w");
+        keyW.press = () => {
+            if (!bunny.jumping) {
+                bunny.jump();
+            }
+
+        }
+    }
+    keyboard(value) {
+        let key = {};
+        key.value = value;
+        key.isDown = false;
+        key.isUp = true;
+        key.press = undefined;
+        key.release = undefined;
+        //The `downHandler`
+        key.downHandler = event => {
+            if (event.key === key.value) {
+                if (key.isUp && key.press) key.press();
+                key.isDown = true;
+                key.isUp = false;
+                event.preventDefault();
+            }
+        };
+
+        //The `upHandler`
+        key.upHandler = event => {
+            if (event.key === key.value) {
+                if (key.isDown && key.release) key.release();
+                key.isDown = false;
+                key.isUp = true;
+                event.preventDefault();
+            }
+        };
+
+        //Attach event listeners
+        const downListener = key.downHandler.bind(key);
+        const upListener = key.upHandler.bind(key);
+
+        window.addEventListener(
+            "keydown", downListener, false
+        );
+        window.addEventListener(
+            "keyup", upListener, false
+        );
+
+        // Detach event listeners
+        key.unsubscribe = () => {
+            window.removeEventListener("keydown", downListener);
+            window.removeEventListener("keyup", upListener);
+        };
+
+        return key;
+    }
+};
+gameState = new GameState();
+bunny = new Bunny();
 
 document.body.appendChild(app.view);
 const container = new PIXI.Container();
 
 app.stage.addChild(container);
-bunny = new Bunny();
+
 bunny.texture.baseTexture.on('loaded', function () {
     bunny.create();
 });
-
-document.onkeydown = function (key) {
-    key = key || window.event;
-    switch (key.keyCode) {
-        case 38: // up 
-
-            bunny.jump(bunny.bunny.y - 50);
-            break;
-        case 40: // down
-            bunny.move(0, 10);
-            break;
-        case 37: // left
-            bunny.move(-10, 0);
-            break;
-        case 39:
-            bunny.move(10, 0);
-            break;
-    }
-};
-
-
-
-
-
-/*
-for (let i = 0; i < 25; i++) {
-
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
-}
-
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
-
-// Listen for animate update
-app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
-});
-*/
