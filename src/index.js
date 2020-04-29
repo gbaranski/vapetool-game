28b128d31bd5febdd4b90fe08d076d8e402e9b7b
Grzegorz Baranski <gbaranski19@gmail.com>
2020-04-29 17:26:27 +0200

    removed console.log

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const app = new PIXI.Application({
     height: $(window).height(),
     backgroundColor: 0x1099bb,
     resolution: window.devicePixelRatio || 1,
-
 });
+const container = new PIXI.Container();
 class Bunny {
     constructor() {
         this.texture = PIXI.Texture.from('src/assets/bunny.png');
@@ -14,7 +14,6 @@ class Bunny {
         this.flipVelocity = 0;
     }
     create() {
-        console.log(this.bunny);
         this.bunny.x = app.renderer.view.width / 2;
         this.bunny.y = app.renderer.view.height - this.bunny.height;
         this.bunny.vx = 0;
@@ -120,7 +119,6 @@ class GameState {
                 element.y += element.vy;
             }
             if (hitTestRectangle(bunny.bunny, element)) {
-                console.log("hit")
                 displayText.addScore(10);
                 container.removeChild(element);
                 fallingObject.fallingObjects = fallingObject.fallingObjects.filter(function (e) {
@@ -164,7 +162,6 @@ class GameState {
         key.isUp = true;
         key.press = undefined;
         key.release = undefined;
-        //The `downHandler`
         key.downHandler = event => {
             if (event.key === key.value) {
                 if (key.isUp && key.press) key.press();
@@ -173,8 +170,6 @@ class GameState {
                 event.preventDefault();
             }
         };
-
-        //The `upHandler`
         key.upHandler = event => {
             if (event.key === key.value) {
                 if (key.isDown && key.release) key.release();
@@ -183,91 +178,62 @@ class GameState {
                 event.preventDefault();
             }
         };
-
-        //Attach event listeners
         const downListener = key.downHandler.bind(key);
         const upListener = key.upHandler.bind(key);
-
         window.addEventListener(
             "keydown", downListener, false
         );
         window.addEventListener(
             "keyup", upListener, false
         );
-
-        // Detach event listeners
         key.unsubscribe = () => {
             window.removeEventListener("keydown", downListener);
             window.removeEventListener("keyup", upListener);
         };
-
         return key;
     }
 };
 
 function hitTestRectangle(r1, r2) {
-
-    //Define the variables we'll need to calculate
     let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
-
-    //hit will determine whether there's a collision
     hit = false;
-
-    //Find the center points of each sprite
     r1.centerX = r1.x + r1.width / 2;
     r1.centerY = r1.y + r1.height / 2;
     r2.centerX = r2.x + r2.width / 2;
     r2.centerY = r2.y + r2.height / 2;
-
-    //Find the half-widths and half-heights of each sprite
     r1.halfWidth = r1.width / 2;
     r1.halfHeight = r1.height / 2;
     r2.halfWidth = r2.width / 2;
     r2.halfHeight = r2.height / 2;
-
-    //Calculate the distance vector between the sprites
     vx = r1.centerX - r2.centerX;
     vy = r1.centerY - r2.centerY;
-
-    //Figure out the combined half-widths and half-heights
     combinedHalfWidths = r1.halfWidth + r2.halfWidth;
     combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-    //Check for a collision on the x axis
     if (Math.abs(vx) < combinedHalfWidths) {
-
-        //A collision might be occurring. Check for a collision on the y axis
         if (Math.abs(vy) < combinedHalfHeights) {
-
-            //There's definitely a collision happening
             hit = true;
         } else {
-
-            //There's no collision on the y axis
             hit = false;
         }
     } else {
-
-        //There's no collision on the x axis
         hit = false;
     }
-
-    //`hit` will be either `true` or `false`
     return hit;
 };
-gameState = new GameState();
-bunny = new Bunny();
-fallingObject = new FallingObject();
-displayText = new DisplayText();
-document.body.appendChild(app.view);
-const container = new PIXI.Container();
 
-app.stage.addChild(container);
+$(document).ready(function() {
+    gameState = new GameState();
+    bunny = new Bunny();
+    fallingObject = new FallingObject();
+    displayText = new DisplayText();
+    document.body.appendChild(app.view);
+    app.stage.addChild(container);
 
-bunny.texture.baseTexture.on('loaded', function () {
-    bunny.create();
+    bunny.texture.baseTexture.on('loaded', function () {
+        bunny.create();
+    });
+    fallingObject.texture.baseTexture.on('loaded', function () {
+        fallingObject.create();
+        displayText.updateScoreText();
+    })
 });
-fallingObject.texture.baseTexture.on('loaded', function () {
-    fallingObject.create();
-    displayText.updateScoreText();
-})
