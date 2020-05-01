0de7f130c42d313a97988951c5b0b202e3ac85f4
gbaranski <gbaranski19@gmail.com>
2020-05-01 23:40:19 +0200

    splitted into separate files, renamed bunny

--- src/index.js
+++ src/index.js
@@ -1,236 +1,37 @@
-class Bunny {
-  constructor(loader) {
-    loader.load((loader, resources) => {
-      this.sprite = new PIXI.Sprite(resources.bunny.texture)
-    });
-    this.jumping = false;
-    this.flipping = false;
-    this.flipVelocity = 0;
-    this.friction = 0.5;
-    this.ax = 0;
-    this.axErrorMargin = 0.1;
-  }
-  create() {
-    this.sprite.x = app.renderer.view.width / 2;
-    this.sprite.y = app.renderer.view.height - this.sprite.height;
-    this.sprite.vx = 0;
-    this.sprite.vy = 0;
-    container.addChild(this.sprite);
-  }
-  jump() {
-    this.sprite.vy = -10;
-  }
-  checkIfOnGround() {
-    return this.sprite.y === app.renderer.view.height - this.sprite.height;
-  }
-  canMoveRight(){
-      return this.sprite.vx < 10 && this.ax > 0
-  }
-  canMoveLeft(){
-    return this.sprite.vx > -10 && this.ax < 0
-}
-  handlePhysics(gravity) {
-    if(this.checkIfOnGround()) {
-        this.friction = 0.5;
-    } else {
-        this.friction = 0.1;
+function keyboard(value) {
+  let key = {};
+  key.value = value;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  key.downHandler = (event) => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
     }
-    if (this.canMoveRight()) {
-      this.sprite.vx += this.ax - this.friction;
+  };
+  key.upHandler = (event) => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
     }
-    if (this.canMoveLeft()) {
-      this.sprite.vx += this.ax + this.friction;
-    }
-    if (this.ax === 0) {
-      if (this.sprite.vx > 0) {
-        this.sprite.vx -= this.friction;
-        if(this.sprite.vx < this.axErrorMargin) {
-          this.sprite.vx = 0;
-        }
-      } else if (this.sprite.vx < 0) {
-        this.sprite.vx += this.friction;
-        if(this.sprite.vx > this.axErrorMargin) {
-          this.sprite.vx = 0;
-        }
-      }
-    }
-    this.sprite.x += this.sprite.vx;
-    this.sprite.y += this.sprite.vy;
-    this.sprite.y = Math.min(
-      this.sprite.y,
-      app.renderer.view.height - this.sprite.height
-    );
-    if (this.sprite.y <= app.renderer.view.height - this.sprite.height) {
-      this.sprite.vy += gravity;
-    } else {
-      this.sprite.vy = 0;
-    }
-    this.ax = Math.floor(this.ax);
-  }
-  handleFlips() {
-    if (this.sprite.vx > 0 && this.flipping == false) {
-      this.flipVelocity = 10;
-    } else if (this.flipping == false) {
-      this.flipVelocity = -10;
-    }
-    if (this.flipping) {
-      this.sprite.angle += this.flipVelocity;
-      if (Math.abs(this.sprite.angle) > 360) {
-        this.flipping = false;
-        this.sprite.angle = 0;
-      }
-    }
-  }
+  };
+  const downListener = key.downHandler.bind(key);
+  const upListener = key.upHandler.bind(key);
+  window.addEventListener("keydown", downListener, false);
+  window.addEventListener("keyup", upListener, false);
+  key.unsubscribe = () => {
+    window.removeEventListener("keydown", downListener);
+    window.removeEventListener("keyup", upListener);
+  };
+  return key;
 }
 
-class FallingObject {
-  constructor(loader) {
-    loader.load((loader, resources) => {
-      this.texture = new PIXI.Texture(resources.fallingObject.texture);
-    });
-    this.fallingObjects = [];
-    this.objectGravity = 3;
-  }
-  create() {
-    for (let i = 0; i < 2; i++) {
-      this.fallingObjects[i] = new PIXI.Sprite(this.texture);
-      this.fallingObjects[i].scale.x = 0.1;
-      this.fallingObjects[i].scale.y = 0.1;
-      this.fallingObjects[i].x = Math.floor(Math.random() * app.renderer.width);
-      this.fallingObjects[i].y = 0;
-      this.fallingObjects[i].vy = this.objectGravity;
-      container.addChild(this.fallingObjects[i]);
-    }
-  }
-  handleFallingObjectsPhysics(sprite, displayText) {
-    this.fallingObjects.forEach((element) => {
-      if (element.y <= app.renderer.view.height - element.height) {
-        element.y += element.vy;
-      }
-      if (hitTestRectangle(sprite, element)) {
-        displayText.addScore(10);
-        container.removeChild(element);
-        this.fallingObjects = this.fallingObjects.filter(
-          (e) => e !== element
-        );
-      }
-    });
-  }
-}
-
-class DisplayText {
-  constructor() {
-    this.scoreText;
-    this.score = 0;
-    this.style = new PIXI.TextStyle({
-      fontFamily: "Arial",
-      fontSize: 36,
-      fill: "white",
-      stroke: "#ff3300",
-      strokeThickness: 4,
-      dropShadow: true,
-      dropShadowColor: "#000000",
-      dropShadowBlur: 4,
-      dropShadowAngle: Math.PI / 6,
-      dropShadowDistance: 6,
-    });
-  }
-  updateScoreText() {
-    container.removeChild(this.scoreText);
-    this.scoreText = new PIXI.Text(`Score: ${this.score}ml`, this.style);
-    this.scoreText.position.set(0, 0);
-    container.addChild(this.scoreText);
-  }
-  addScore(amount) {
-    this.score += amount;
-    this.updateScoreText();
-  }
-  addCenterText(text) {
-    this.scoreText = new PIXI.Text(text, this.style);
-    this.scoreText.position.set(
-      app.renderer.view.width / 2,
-      app.renderer.height / 2
-    );
-    container.addChild(this.scoreText);
-  }
-}
-
-class GameState {
-  constructor(bunny, fallingObject, displayText) {
-    this.bunny = bunny;
-    this.fallingObject = fallingObject;
-    this.displayText = displayText;
-    this.gravity = 0.5;
-    this.handleKeyboardPress();
-  }
-  gameLoop() {
-    console.log(this.bunny.sprite.vx);
-    this.bunny.handlePhysics(this.gravity);
-    this.bunny.handleFlips();
-    this.fallingObject.handleFallingObjectsPhysics(this.bunny.sprite, this.displayText);
-  }
-
-
-  handleKeyboardPress() {
-    let keyA = this.keyboard("a");
-    keyA.press = () => {
-      this.bunny.ax = -1;
-    };
-    keyA.release = () => {
-      this.bunny.ax = 0;
-    };
-
-    let keyD = this.keyboard("d");
-    keyD.press = () => {
-      this.bunny.ax = 1;
-    };
-    keyD.release = () => {
-      this.bunny.ax = 0;
-    };
-
-    let keyW = this.keyboard("w");
-    keyW.press = () => {
-      this.bunny.jump();
-    };
-    let keySpace = this.keyboard(" ");
-    keySpace.press = () => {
-      this.bunny.flipping = true;
-    };
-  }
-  keyboard(value) {
-    let key = {};
-    key.value = value;
-    key.isDown = false;
-    key.isUp = true;
-    key.press = undefined;
-    key.release = undefined;
-    key.downHandler = (event) => {
-      if (event.key === key.value) {
-        if (key.isUp && key.press) key.press();
-        key.isDown = true;
-        key.isUp = false;
-        event.preventDefault();
-      }
-    };
-    key.upHandler = (event) => {
-      if (event.key === key.value) {
-        if (key.isDown && key.release) key.release();
-        key.isDown = false;
-        key.isUp = true;
-        event.preventDefault();
-      }
-    };
-    const downListener = key.downHandler.bind(key);
-    const upListener = key.upHandler.bind(key);
-    window.addEventListener("keydown", downListener, false);
-    window.addEventListener("keyup", upListener, false);
-    key.unsubscribe = () => {
-      window.removeEventListener("keydown", downListener);
-      window.removeEventListener("keyup", upListener);
-    };
-    return key;
-  }
-}
 
 function hitTestRectangle(r1, r2) {
   let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
@@ -259,6 +60,51 @@ function hitTestRectangle(r1, r2) {
   return hit;
 }
 
+
+class GameState {
+  constructor(player, fallingObject, displayText) {
+    this.player = player;
+    this.fallingObject = fallingObject;
+    this.displayText = displayText;
+    this.gravity = 0.5;
+    this.handleKeyboardPress();
+  }
+  gameLoop() {
+    console.log(this.player.sprite.vx);
+    this.player.handlePhysics(this.gravity);
+    this.player.handleFlips();
+    this.fallingObject.handleFallingObjectsPhysics(this.player.sprite, this.displayText);
+  }
+
+  handleKeyboardPress() {
+    let keyA = keyboard("a");
+    keyA.press = () => {
+      this.player.ax = -1;
+    };
+    keyA.release = () => {
+      this.player.ax = 0;
+    };
+
+    let keyD = keyboard("d");
+    keyD.press = () => {
+      this.player.ax = 1;
+    };
+    keyD.release = () => {
+      this.player.ax = 0;
+    };
+
+    let keyW = keyboard("w");
+    keyW.press = () => {
+      this.player.jump();
+    };
+    let keySpace = keyboard(" ");
+    keySpace.press = () => {
+      this.player.flipping = true;
+    };
+  }
+}
+
+
 const app = new PIXI.Application({
   width: $(window).width(),
   height: $(window).height(),
@@ -270,17 +116,17 @@ const container = new PIXI.Container();
 
 $(document).ready(function () {
   const loader = PIXI.Loader.shared;
-  loader.add('bunny', 'src/assets/bunny.png');
+  loader.add('player', 'src/assets/bunny.png');
   loader.add('fallingObject', 'src/assets/eliquid.png');
-  const bunny = new Bunny(loader);
+  const player = new Player(loader);
   const fallingObject = new FallingObject(loader);
   const displayText = new DisplayText();
-  const gameState = new GameState(bunny, fallingObject, displayText);
+  const gameState = new GameState(player, fallingObject, displayText);
   document.body.appendChild(app.view);
   app.stage.addChild(container);
       loader.onComplete.add(() => {
         fallingObject.create();
-        bunny.create();
+        player.create();
         displayText.updateScoreText();
         app.ticker.add((delta) => gameState.gameLoop(delta));
     });
---
+++ src/objects/player.js
@@ -0,0 +1,85 @@
+class Player {
+    constructor(loader) {
+      loader.load((loader, resources) => {
+        this.sprite = new PIXI.Sprite(resources.player.texture)
+      });
+      this.jumping = false;
+      this.flipping = false;
+      this.flipVelocity = 0;
+      this.friction = 0.5;
+      this.ax = 0;
+      this.axErrorMargin = 0.1;
+    }
+    create() {
+      this.sprite.x = app.renderer.view.width / 2;
+      this.sprite.y = app.renderer.view.height - this.sprite.height;
+      this.sprite.vx = 0;
+      this.sprite.vy = 0;
+      container.addChild(this.sprite);
+    }
+    jump() {
+      this.sprite.vy = -10;
+    }
+    checkIfOnGround() {
+      return this.sprite.y === app.renderer.view.height - this.sprite.height;
+    }
+    canMoveRight(){
+        return this.sprite.vx < 10 && this.ax > 0
+    }
+    canMoveLeft(){
+      return this.sprite.vx > -10 && this.ax < 0
+  }
+    handlePhysics(gravity) {
+      if(this.checkIfOnGround()) {
+          this.friction = 0.5;
+      } else {
+          this.friction = 0.1;
+      }
+      if (this.canMoveRight()) {
+        this.sprite.vx += this.ax - this.friction;
+      }
+      if (this.canMoveLeft()) {
+        this.sprite.vx += this.ax + this.friction;
+      }
+      if (this.ax === 0) {
+        if (this.sprite.vx > 0) {
+          this.sprite.vx -= this.friction;
+          if(this.sprite.vx < this.axErrorMargin) {
+            this.sprite.vx = 0;
+          }
+        } else if (this.sprite.vx < 0) {
+          this.sprite.vx += this.friction;
+          if(this.sprite.vx > this.axErrorMargin) {
+            this.sprite.vx = 0;
+          }
+        }
+      }
+      this.sprite.x += this.sprite.vx;
+      this.sprite.y += this.sprite.vy;
+      this.sprite.y = Math.min(
+        this.sprite.y,
+        app.renderer.view.height - this.sprite.height
+      );
+      if (this.sprite.y <= app.renderer.view.height - this.sprite.height) {
+        this.sprite.vy += gravity;
+      } else {
+        this.sprite.vy = 0;
+      }
+      this.ax = Math.floor(this.ax);
+    }
+    handleFlips() {
+      if (this.sprite.vx > 0 && this.flipping == false) {
+        this.flipVelocity = 10;
+      } else if (this.flipping == false) {
+        this.flipVelocity = -10;
+      }
+      if (this.flipping) {
+        this.sprite.angle += this.flipVelocity;
+        if (Math.abs(this.sprite.angle) > 360) {
+          this.flipping = false;
+          this.sprite.angle = 0;
+        }
+      }
+    }
+  }
+  
