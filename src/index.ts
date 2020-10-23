780bba8e56d2b4fbc3915a9ef072d382874e9a42
gbaranski <gbaranski19@gmail.com>
2020-10-23 18:06:22 +0200

    Remove jquery

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import * as PIXI from 'pixi.js';
-import $ from 'jquery';
 import GameState from './gameState';
 
 import playerImg from './assets/bunny.png';
@@ -15,8 +14,8 @@ import bombButtonImg from './assets/bomb-button.png';
 import shopImg from './assets/shop.png';
 
 const app = new PIXI.Application({
-  width: $(window).width(),
-  height: $(window).height(),
+  width: window.innerWidth,
+  height: window.innerHeight,
   transparent: false,
   backgroundColor: 0x1099bb,
   resolution: 1,
@@ -25,38 +24,36 @@ const app = new PIXI.Application({
 const container = new PIXI.Container();
 export const explosionFrames = Object.values(explosions);
 
-$(document).ready(() => {
-  const loader = PIXI.Loader.shared;
-  loader.add('player', playerImg);
-  loader.add('enemy', enemyImg);
-  loader.add('fallingObject', fallingObjectImg);
-  loader.add('bullet', bulletImg);
-  loader.add('cloud', cloudImg);
-  loader.add('bomb', bombImg);
-  loader.add('bodyguard', bodyguardImg);
-  loader.add('bombButton', bombButtonImg);
-  loader.add('cloudButton', bombButtonImg);
-  loader.add('shop', shopImg);
-  // TODO consider removing
-  explosionFrames.forEach((frame: string) => loader.add(frame, frame));
+const loader = PIXI.Loader.shared;
+loader.add('player', playerImg);
+loader.add('enemy', enemyImg);
+loader.add('fallingObject', fallingObjectImg);
+loader.add('bullet', bulletImg);
+loader.add('cloud', cloudImg);
+loader.add('bomb', bombImg);
+loader.add('bodyguard', bodyguardImg);
+loader.add('bombButton', bombButtonImg);
+loader.add('cloudButton', bombButtonImg);
+loader.add('shop', shopImg);
+// TODO consider removing
+explosionFrames.forEach((frame: string) => loader.add(frame, frame));
 
-  const sprites: any = {};
-  loader.load((_loader, resources) => {
-    sprites.player = new PIXI.Sprite(resources.player.texture);
-    sprites.enemy = new PIXI.Sprite(resources.enemy.texture);
-    sprites.fallingObject = new PIXI.Sprite(resources.fallingObject.texture);
-    sprites.bullet = new PIXI.Sprite(resources.bullet.texture);
-    sprites.cloud = new PIXI.Sprite(resources.cloud.texture);
-    sprites.bomb = new PIXI.Sprite(resources.bomb.texture);
-    sprites.bodyguard = new PIXI.Sprite(resources.bodyguard.texture);
-    sprites.bombButton = new PIXI.Sprite(resources.bombButton.texture);
-    sprites.cloudButton = new PIXI.Sprite(resources.cloudButton.texture);
-    sprites.shop = new PIXI.Sprite(resources.shop.texture);
-  });
-  loader.onComplete.add(() => {
-    const gameState = new GameState(container, app, explosionFrames, sprites);
-    document.body.appendChild(app.view);
-    app.stage.addChild(container);
-    app.ticker.add((delta) => gameState.gameLoop(delta));
-  });
+const sprites: any = {};
+loader.load((_loader, resources) => {
+  sprites.player = new PIXI.Sprite(resources.player.texture);
+  sprites.enemy = new PIXI.Sprite(resources.enemy.texture);
+  sprites.fallingObject = new PIXI.Sprite(resources.fallingObject.texture);
+  sprites.bullet = new PIXI.Sprite(resources.bullet.texture);
+  sprites.cloud = new PIXI.Sprite(resources.cloud.texture);
+  sprites.bomb = new PIXI.Sprite(resources.bomb.texture);
+  sprites.bodyguard = new PIXI.Sprite(resources.bodyguard.texture);
+  sprites.bombButton = new PIXI.Sprite(resources.bombButton.texture);
+  sprites.cloudButton = new PIXI.Sprite(resources.cloudButton.texture);
+  sprites.shop = new PIXI.Sprite(resources.shop.texture);
+});
+loader.onComplete.add(() => {
+  const gameState = new GameState(container, app, explosionFrames, sprites);
+  document.body.appendChild(app.view);
+  app.stage.addChild(container);
+  app.ticker.add((delta) => gameState.gameLoop(delta));
 });
